03af87e58143d276393fd535624ac8a56a28c52c
Yi Zheng Tang <g00425075@atu.ie>
2024-04-22 08:39:57 +0800

    added some stuff in sql

---
+++ src/test/java/ie/atu/MenusTest.java
@@ -0,0 +1,41 @@
+package ie.atu;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class MenusTest {
+    Menus Menu;
+    @Test
+    //Test the LogInMenu
+    void testMenu1(){
+        Menu = new Menus();
+        String testMenu = ("(1)\t Sign in as User\n(2)\t Sign in as Staff\n(3)\t Sign up new User\n\n(0)\t Exit");
+        assertEquals(testMenu, Menu.LogInMenu());
+    }
+
+    @Test
+    //Test the customerMenu
+    void testMenu2(){
+        Menu = new Menus();
+        String testMenu = ("(1)\t View Product\n(2)\t View Stores\n(3)\t Edit Personal Info\n(4)\t Delete Account!\n\n(0)\t Logout");
+        assertEquals(testMenu, Menu.customerMenu());
+    }
+
+    @Test
+    //Test the staffMenu
+    void testMenu3(){
+        Menu = new Menus();
+        String testMenu = ("(1)\t Product\n(2)\t Store\n(3)\t Staff\n(4)\t View customer\n(5)\t Delete Account!\n\n(0)\t Logout");
+        assertEquals(testMenu, Menu.staffMenu());
+    }
+
+    @Test
+    //Test the selectionMenu
+    void testMenu4(){
+        Menu = new Menus();
+        String testMenu = ("(1)\t Create new\n(2)\t Read from existing\n(3)\t Edit existing\n(4)\t Delete from existing\n\n(0)\t Exit");
+        assertEquals(testMenu, Menu.selectionMenu());
+    }
+}
