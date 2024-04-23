754051f95e21e87a4c52c8b691f5e7272fb59f77
yizhengtang <118446356+yizhengtang@users.noreply.github.com>
2024-04-23 11:15:04 +0100

    This is the final version of the database for submission

--- shoedatabase.sql
+++ shoedatabase.sql
@@ -29,7 +29,7 @@ CREATE TABLE `address` (
   `city` varchar(45) NOT NULL,
   `country` varchar(45) NOT NULL,
   PRIMARY KEY (`address_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -38,7 +38,7 @@ CREATE TABLE `address` (
 
 LOCK TABLES `address` WRITE;
 /*!40000 ALTER TABLE `address` DISABLE KEYS */;
-INSERT INTO `address` VALUES (1,'Eyre Square','h123','Galway','Ireland'),(2,'ATU','h111','Galway','Ireland'),(3,'Glasan','abcd','Galway','Ireland'),(4,'Renmore','qwer','Galway','Ireland'),(5,'abc','defg','Galway','Ireland'),(6,'atu','g123','Galway','Ireland'),(7,'tuam','g345','Galway','Ireland'),(8,'glasan','g004','Galway','Ireland'),(9,'lios','cxy','Galway','Ireland'),(10,'lakers','nba','Los Angeles','US'),(11,'somewhere','xyz','New York','US');
+INSERT INTO `address` VALUES (1,'Eyre Square','h123','Galway','Ireland'),(2,'ATU','h111','Galway','Ireland'),(3,'Glasan','abcd','Galway','Ireland'),(4,'Renmore','qwer','Galway','Ireland'),(5,'abc','defg','Galway','Ireland'),(6,'atu','g123','Galway','Ireland'),(7,'tuam','g345','Galway','Ireland'),(8,'glasan','g004','Galway','Ireland'),(9,'lios','cxy','Galway','Ireland'),(10,'lakers','nba','Los Angeles','US'),(11,'somewhere','xyz','New York','US'),(12,'galway','h234','galway','ireland'),(13,'ATU','h234','Galway','Ireland'),(14,'ATU','h91','Galway','Ireland'),(15,'atu','h09','Galway','Ireland'),(16,'ATU','h91','galway','ireland');
 /*!40000 ALTER TABLE `address` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -82,7 +82,7 @@ CREATE TABLE `customer` (
   PRIMARY KEY (`customer_id`),
   KEY `fk_customer_address_idx` (`address_id`),
   CONSTRAINT `fk_customer_address` FOREIGN KEY (`address_id`) REFERENCES `address` (`address_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -91,7 +91,7 @@ CREATE TABLE `customer` (
 
 LOCK TABLES `customer` WRITE;
 /*!40000 ALTER TABLE `customer` DISABLE KEYS */;
-INSERT INTO `customer` VALUES (1,5,'Johnny','leeren','johnny123'),(2,8,'Drake','aubrey','drizzy'),(3,10,'Lebron','james','kingjames'),(4,11,'Mike Tyson','miket','tyson123');
+INSERT INTO `customer` VALUES (1,5,'Johnny','leeren','johnny123'),(2,8,'Drake','aubrey','drizzy'),(3,10,'Lebron','james','kingjames'),(4,11,'Mike Tyson','mikey','tyson123'),(5,12,'Bob Marley','marley','marley123'),(6,13,'Adam','adam@gmail.com','adam123'),(7,14,'Adam Silver','silver@gmail.com','silver123'),(8,16,'Curry','curry@gmail.com','steph123');
 /*!40000 ALTER TABLE `customer` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -117,7 +117,7 @@ CREATE TABLE `inventory` (
   CONSTRAINT `fk_inventory_product` FOREIGN KEY (`product_id`) REFERENCES `product` (`product_id`),
   CONSTRAINT `fk_inventory_size` FOREIGN KEY (`size_id`) REFERENCES `size` (`size_id`),
   CONSTRAINT `fk_inventory_store` FOREIGN KEY (`store_id`) REFERENCES `store` (`store_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=51 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -126,7 +126,7 @@ CREATE TABLE `inventory` (
 
 LOCK TABLES `inventory` WRITE;
 /*!40000 ALTER TABLE `inventory` DISABLE KEYS */;
-INSERT INTO `inventory` VALUES (1,1,1,4,5),(2,1,1,4,6),(3,1,2,4,7),(4,1,2,4,8),(5,2,1,1,8),(6,2,1,1,8),(7,2,1,1,9),(8,2,1,1,8),(9,2,1,1,9),(10,2,2,2,7),(11,2,2,2,7),(12,2,2,2,8),(13,2,2,2,7),(14,2,2,2,8),(15,3,3,9,10),(16,3,3,9,10),(17,3,3,10,4),(18,3,3,10,4),(19,3,3,10,5),(20,3,3,10,4),(21,3,3,10,5),(22,3,3,10,6),(23,3,3,10,4),(24,3,3,10,5),(25,3,3,10,6),(26,4,2,4,8),(27,4,2,4,8),(28,4,2,4,9),(29,4,2,4,8),(30,4,2,4,9),(31,4,2,8,8),(32,4,2,8,8),(33,4,2,8,9),(34,4,2,8,8),(35,4,2,8,9),(36,4,3,7,12),(37,4,3,7,12),(38,4,3,7,13),(39,4,3,7,12),(40,4,3,7,13);
+INSERT INTO `inventory` VALUES (1,1,1,4,5),(2,1,1,4,6),(3,1,2,4,7),(4,1,2,4,8),(5,2,1,1,8),(6,2,1,1,8),(7,2,1,1,9),(8,2,1,1,8),(9,2,1,1,9),(10,2,2,2,7),(11,2,2,2,7),(12,2,2,2,8),(13,2,2,2,7),(14,2,2,2,8),(15,3,3,9,10),(16,3,3,9,10),(17,3,3,10,4),(18,3,3,10,4),(19,3,3,10,5),(20,3,3,10,4),(21,3,3,10,5),(22,3,3,10,6),(23,3,3,10,4),(24,3,3,10,5),(25,3,3,10,6),(26,4,2,4,8),(27,4,2,4,8),(28,4,2,4,9),(29,4,2,4,8),(30,4,2,4,9),(31,4,2,8,8),(32,4,2,8,8),(33,4,2,8,9),(34,4,2,8,8),(35,4,2,8,9),(36,4,3,7,12),(37,4,3,7,12),(38,4,3,7,13),(39,4,3,7,12),(40,4,3,7,13),(41,5,1,1,1),(42,5,1,1,1),(43,5,1,2,2),(44,5,1,2,2),(45,5,2,1,1),(46,5,2,1,1),(47,6,4,1,1),(48,6,4,1,1),(49,6,4,2,5),(50,6,4,2,5);
 /*!40000 ALTER TABLE `inventory` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -155,7 +155,7 @@ CREATE TABLE `product` (
 
 LOCK TABLES `product` WRITE;
 /*!40000 ALTER TABLE `product` DISABLE KEYS */;
-INSERT INTO `product` VALUES (1,'Jordan 1 Retro High Spiderman Origin Story','Jordan',160.00,'2018-12-14','This AJ1 comes with a white upper plus red accents, black Nike \"Swoosh\", white midsole, and translucent sole.','out of stock'),(2,'Air Force 1','Nike',100.00,'2003-01-01','This is an Air Force 1','In stock'),(3,'Slippers','Nike',200.00,'2001-01-01','This is the most expensive slippers ever existed','In stock'),(4,'Dunk High','Nike',100.00,'2005-05-05','This is nike dunk highs','In stock');
+INSERT INTO `product` VALUES (1,'Jordan 1 Retro High Spiderman Origin Story','Jordan',160.00,'2018-12-14','This AJ1 comes with a white upper plus red accents, black Nike \"Swoosh\", white midsole, and translucent sole.','out of stock'),(2,'Air Force 1','Nike',100.00,'2003-01-01','This is an Air Force 1','In stock'),(3,'Slippers','Nike',200.00,'2001-01-01','This is the most expensive slippers ever existed','In stock'),(4,'Dunk High','Nike',100.00,'2005-05-05','This is nike dunk highs','In stock'),(5,'Slippers','nike',20.00,'2024-04-23','This is a slippers','In Stock'),(6,'Jordan 2','nike',200.00,'2024-04-23','This is jordan 2','In stock');
 /*!40000 ALTER TABLE `product` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -229,7 +229,7 @@ CREATE TABLE `store` (
   PRIMARY KEY (`store_id`),
   KEY `fk_store_address_idx` (`address_id`),
   CONSTRAINT `fk_store_address` FOREIGN KEY (`address_id`) REFERENCES `address` (`address_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -238,7 +238,7 @@ CREATE TABLE `store` (
 
 LOCK TABLES `store` WRITE;
 /*!40000 ALTER TABLE `store` DISABLE KEYS */;
-INSERT INTO `store` VALUES (1,1,'Minecraft'),(2,2,'Roblox'),(3,7,'Fortnite');
+INSERT INTO `store` VALUES (1,1,'Minecraft'),(2,2,'Roblox'),(3,7,'Fortnite'),(4,15,'ATU');
 /*!40000 ALTER TABLE `store` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -251,4 +251,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-04-22  8:42:08
+-- Dump completed on 2024-04-23 18:14:24
